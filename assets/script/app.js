47286c764f2e9f9d8f97e75e21b54a48bf8744c5
daniel zhang <traceless0714@gmail.com>
2025-11-20 08:52:04 -0600

    changed the background for contactpage

--- assets/script/app.js
+++ assets/script/app.js
@@ -1,15 +1,6 @@
 'use strict';
-import { getElement, listen } from './utils.js';
 
 document.addEventListener("DOMContentLoaded", () => {
-const backToTopBtn = getElement('backToTop');
-
-listen('click', backToTopBtn, () => {
-    window.scrollTo({
-        top: 0,
-        behavior: 'smooth'
-    });
-});
 
 const backtotopbtn = document.querySelector(".backtotop");
 const herobanner = document.getElementById("herobanner");
