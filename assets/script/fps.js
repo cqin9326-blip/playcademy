cd65d5beeab8e2c8e3a6ddb41a501ae9cc01d82d
daniel zhang <traceless0714@gmail.com>
2025-11-20 10:26:48 -0600

    add login to all pages

--- assets/script/fps.js
+++ assets/script/fps.js
@@ -81,4 +81,18 @@ document.addEventListener("DOMContentLoaded", () => {
   fpsbanner.addEventListener("mouseleave", () => {
     backtotopbtn.classList.remove("off");
   })
+
+    const loginclose = document.getElementById("login-close");
+    const profile = document.querySelector(".profile");
+    const login = document.querySelector(".login");
+
+    profile.addEventListener("click", () => {
+        login.classList.toggle("invisible");
+        loginclose.classList.toggle("invisible");
+    })
+
+    loginclose.addEventListener("click", () => {
+        login.classList.toggle("invisible");
+        loginclose.classList.toggle("invisible");
+    })
 });
